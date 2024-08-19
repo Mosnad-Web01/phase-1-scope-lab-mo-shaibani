8d91d17ae159d035667d8d17cc102254189141b6
mohammed-alshaibani <20160129@su.edu.ye>
2024-08-19 13:34:52 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+     customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+ return globalThis.bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+
+
+const leastFavoriteCustomer = "M Shaibani";
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = "Mohand"
+}
